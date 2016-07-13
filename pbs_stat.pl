ab207a1ce3648d1629ae42520d78c8a520c7c3e8
yadavmahi <yadavmahe@hotmail.com>
2016-07-13 16:52:51 +0530

    New update for PBS_STAT
    Server Details and Queue details display
    
    Job wise search added.

--- pbs_stat.pl
+++ pbs_stat.pl
@@ -6,19 +6,33 @@ use strict;
 sub  trim {my $s = shift; if (defined $s){ $s =~ s/^\s+|\s+$//g;}else{$s="";} return $s };
 
 
-#my @output=`/opt/pbs/default/bin/qstat -f 304628 304627 304592 304591`;
 my $cmd;
+my $cmd1;
+my $cmd2;
+my $option=" -f";
+my $option1=" -Bf";
+my $option2=' -q  | sed \'1,5d\' | awk \'{print $1," ",$6," ",$7," ",$8}\'';
+#my $option=" -x -f";
 if (defined $ENV{'QSTAT'} ){
-$cmd=$ENV{'QSTAT'}+" -f";
+$cmd=$ENV{'QSTAT'}.$option;
+$cmd1=$ENV{'QSTAT'}.$option1;
+$cmd2=$ENV{'QSTAT'}.$option2;
 }else{
-$cmd="/opt/pbs/default/bin/qstat -f";
+$cmd="/opt/pbs/default/bin/qstat ".$option;
+$cmd1="/opt/pbs/default/bin/qstat ".$option1;
+$cmd2="/opt/pbs/default/bin/qstat ".$option2;
 }
 
 #my @output=`/opt/pbs/default/bin/qstat -f`;
 #print $cmd;
 my @output=`$cmd`;
+my @output1=`$cmd1`;
+my @output2=`$cmd2`;
+#my @output=`/opt/pbs/default/bin/qstat -f 304628 304627 304592 304591`;
 
 chomp(@output);  # remove new lines
+chomp(@output1); # remove new lines
+chomp(@output2);  # remove new lines
 
 my $sitetitle="PBS Stat";
 my $linestart = 0;
@@ -29,15 +43,88 @@ my $jobstate="";
 my $queue="";
 my $cores="";
 my $mem="";
+my $node="";
 my $date = localtime();
+
+my $server_name="";
+my $server_state="";
+my $server_amem="";
+my $server_acores="";
+
+my $queue_name="";
+my $queue_mem="";
+my $queue_cpu="";
+my $queue_wall="";
+my $queue_node="";
+my $queue_run="";
+my $queue_que="";
+my $queue_lim="";
+my $queue_state="";
 
 printf("<html><head><title>%s</title><script src=\"jquery-3.0.0.min.js\"></script><script src=\"script.js\"></script> <link href=\"styles.css\" rel=\"stylesheet\"/></head>\n",$sitetitle);
 printf("<body><div class=\"content\">\n");
-printf("<div class=\"header\"><h1> %s </h1><span class=\"time\">%s IST</span></div><div class=\"center\">",$sitetitle,$date);
-printf("<div class=\"left\"><h2> Details </h2><div class=\"res\"></div></div>");
-printf("<div class=\"right\"><h2> Jobs </h2><div id=\"buttons\"><span class=\"run\">Running</span><span class=\"que\">Waiting</span><span class=\"err\">Error</span></div>");
+printf("<div class=\"header\"><h1> %s </h1><span class=\"time\">%s IST</span></div>",$sitetitle,$date);
+printf("<div class=\"center\">");
+printf("<div class=\"tabs\"><span class=\"system\"> Server Details </span> <span class=\"details\"> Job Details </span></div>");
 
-printf("<table id=\"main\"><thead><tr><th>Job ID</th><th>Job Name</th><th>Job Owner</th><th>Job State</th><th>Queue</th><th>Cores</th><th>Memory</th></tr></thead>\n");
+printf("<div class=\"left\">");
+printf("<div class=\"dmain1\">");
+printf("<table id=\"smain\"><thead><tr><th>Servername</th><th>Status</th><th>Assigned Cores</th><th>Assigned Memory</th></tr></thead>\n");
+
+foreach my $line(@output1){
+#   print $line."\n";
+	if ($line =~ /^$/){
+#		print "hellop\n";
+	}else{
+	if ( $line =~ /^Server/ ){
+if($linestart > 0) {
+printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",$server_name,$server_state,$server_acores,$server_amem) ; 
+}
+$linestart=1;
+$server_name="";
+$server_state="";
+$server_amem="";
+$server_acores="";
+
+	my ($attribute1,$value1)=split /:/,trim($line);
+        $server_name=trim($value1);
+# 	print ".....ID>..".$jobid;
+	}else{
+#print $line."\n";
+	  my ($attribute1,$value1)=split /=/,trim($line);
+	  my $attribute=trim($attribute1);
+	  my $value=trim($value1);
+	  #print $attribute."...".$value."\n";
+	  if($attribute eq "server_state"){ $server_state=$value;}
+	  if($attribute eq "resources_assigned.mem"){ $server_amem=$value;}
+	  if($attribute eq "resources_assigned.ncpus"){ $server_acores=$value;}
+	}
+	}
+}
+printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",$server_name,$server_state,$server_acores,$server_amem) ; 
+printf("</table>\n");
+
+printf("<table id=\"qmain\"><thead><tr><th>Queue</th><th>Running jobs</th><th>Queue jobs</th><th>Limit cores</th></tr></thead>\n");
+
+foreach my $line(@output2){
+#   print $line."\n";
+	if ($line =~ /^$/){
+#		print "hellop\n";
+	}else{
+	if ( $line =~ /^[a-z,A-Z]/ ){
+#		print "$line\n";
+		my @queue_fields=split / /,trim($line);
+		printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",$queue_fields[0],$queue_fields[3],$queue_fields[6],$queue_fields[9]) ;
+	}
+}
+}
+printf("</table></div>\n");
+
+printf("<div class=\"dmain2\">");
+printf("<div class=\"res\"></div><div class=\"search\"></div>");
+printf("</div></div>");
+printf("<div class=\"right\"><h2> Jobs </h2><div class=\"res\"><span></span></div>");
+printf("<table id=\"main\"><thead><tr><th>Job ID</th><th>Job Name</th><th>Job Owner</th><th>Job State</th><th>Queue</th><th>Cores</th><th>Memory</th><th>Node</th></tr></thead>\n");
 
 foreach my $line(@output){
 #	print $line."\n";
@@ -48,7 +135,7 @@ foreach my $line(@output){
 #	  print "hai\n";
 #printf("%10s%10s%10s%20s%10s%25s%20s\t%30s\n",$sys,$stat,$cores,$mem,$ucores,$umem,$acores,$jobs);
 if($linestart > 0) {
-printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",$jobid,$jobname,$jobowner,$jobstate,$queue,$cores,$mem) ; 
+printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",$jobid,$jobname,$jobowner,$jobstate,$queue,$cores,$mem,$node) ; 
 }
 $linestart=1;
 $jobid="";
@@ -58,6 +145,7 @@ $jobstate="";
 $queue="";
 $cores="";
 $mem="";
+$node="";
 	my ($attribute1,$value1)=split /:/,trim($line);
         $jobid=trim($value1);
 # 	print ".....ID>..".$jobid;
@@ -73,10 +161,11 @@ $mem="";
 	  if($attribute eq "queue"){ $queue=$value;}
 	  if($attribute eq "Resource_List.ncpus"){ $cores=$value;}
 	  if($attribute eq "Resource_List.mem"){ $mem=$value;}
+	  if($attribute eq "exec_host"){ $node=$value;}
 	}
 	}
 }
-printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",$jobid,$jobname,$jobowner,$jobstate,$queue,$cores,$mem) ; 
+printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>\n",$jobid,$jobname,$jobowner,$jobstate,$queue,$cores,$mem,$node) ; 
 printf("</table></div></div>\n");
 printf("<div class=\"footer\"><h3>Copy right \@ Mahesh Yadav</h3></div>\n");
 printf("</div></body>\n");
--- script.js
+++ script.js
@@ -3,24 +3,32 @@ $( document ).ready(function() {
 var queues = [];
 var queuescount=[];
 var queuescores=[];
+var queuescomment="";
 var states = [];
 var statescount=[];
+var statescores=[];
+var statescomment="";
 var coresused=0;
+var jobsused=0;
 
 $.getJSON( "data.json", function( jdt ) {
+console.log(jdt);
 //alert("....."+jd);
-
   $.each( jdt.queue, function( key, val ) {
 //alert(val);
     queues.push(val);
     queuescount.push(0);
     queuescores.push(0);
   });
+//alert(jdt.qcomment);
+  queuescomment=jdt.qcomment;
   $.each( jdt.state, function( key, val ) {
 //alert(val);
     states.push(val);
     statescount.push(0);
+    statescores.push(0);
   });
+  statescomment=jdt.scomment;
 }).done(function() {
     console.log( "second success" );
 
@@ -34,6 +42,7 @@ num=parseInt(data2)?data2:0;
 for ( var i =0 ;i < states.length ;i++){
 if (states[i] == data) { 
 statescount[i]=statescount[i]+1;
+statescores[i]=parseInt(statescores[i])+parseInt(num);
  }
 }
 
@@ -44,61 +53,116 @@ queuescores[i]=parseInt(queuescores[i])+parseInt(num);
  }
 }
 
+if(num){
 //coresused=coresused+parseInt(data2);
 coresused=parseInt(coresused)+parseInt(num);
+jobsused=parseInt(jobsused)+1;
+}
 
 });
 
-out1='<table class="restable"><thead><tr><th>State</th><th>Job Count</th></tr></thead>';
+out1='<table class="restable"><thead><tr><th>State</th><th>Job Count</th><th>Cores Count</th></tr></thead>';
 //alert(states.length);
 for ( var i =0 ;i < states.length ;i++){
-out1=out1+'<tr><td>'+states[i]+'</td><td>'+statescount[i]+'</td></tr>'
+out1=out1+'<tr><td class=\"bt\">'+states[i]+'</td><td>'+statescount[i]+'</td><td>'+statescores[i]+'</td></tr>'
 }
-out1=out1+'<tr><td colspan=2><span class="note">* : R - Running,<br> Q - Waiting state ,<br> E - Error state </span></td></table>';
+out1=out1+'<tr><td>Total</td><td>'+jobsused+'</td><td>'+coresused+'</td></tr>'
+out1=out1+'<tr><td colspan=3><span class="note">'+statescomment+' </span></td></table>';
 
-out2='<table class="restable"><thead><tr><th>Queue</th><th>Job Count</th></tr></thead>';
+out2='<table class="restable"><thead><tr><th>Queue</th><th>Job Count</th><th>Cores Count</th></tr></thead>';
 //alert(queues.length);
 for ( var i =0 ;i < queues.length ;i++){
-out2=out2+'<tr><td>'+queues[i]+'</td><td>'+queuescount[i]+'</td></tr>'
+out2=out2+'<tr><td class=\"bt1\">'+queues[i]+'</td><td>'+queuescount[i]+'</td><td>'+queuescores[i]+'</td></tr>'
 }
-out2=out2+'</table>';
+out2=out2+'<tr><td>Total</td><td>'+jobsused+'</td><td>'+coresused+'</td></tr>'
+out2=out2+'<tr><td colspan=3><span class="note">'+queuescomment+' </span></td></table>';
 
-out3='<table class="restable"><thead><tr><th>Alloted Cores</th><th>'+coresused+'</th></tr></thead>';
-for ( var i =0 ;i < queues.length ;i++){
-out3=out3+'<tr><td>'+queues[i]+'</td><td>'+queuescores[i]+'</td></tr>'
-}
-out3=out3+'</table>';
+$('div.left div.res').html(out1+out2);
 
-$('div.left div.res').html(out1+out2+out3);
+var i=1;
+out5='<span> Search :  </span><select>';
+$('#main thead tr th').each(function(){
+//alert($(this).text());
+out5=out5+'<option value="'+i+'">'+$(this).text()+'</option>';
+i=i+1;
+});
+out5=out5+'</select><input class="sinp" type="text"></input>';
+$('div.left div.search').html(out5);
 
 
 });
 
+function show_table(){
+$('#main tr').each(function(){ $(this).show(); });
+}
 
-// Buttons script
-$("#buttons span").click(function(){
 function table_hide(showval){
 //alert(showval);
-$('main tr').show();
+show_table();
 $('#main tr').each(function(){
 chk=$.trim($(this).children("td:nth-child(4)").text());
 if(chk === showval){ $(this).show(); }else{ $(this).hide(); }
 });
 $('#main thead tr').show();
 }
-
-var id = $(this).attr("class");
-//alert("hai"+id);
-if(id == "run"){
-table_hide('R');
-}
-if(id == "que"){
-table_hide('Q');
-}
-if(id == "err"){
-table_hide('E');
-}
+function table_hide1(showval){
+//alert(showval);
+show_table();
+$('#main tr').each(function(){
+chk=$.trim($(this).children("td:nth-child(5)").text());
+if(chk === showval){ $(this).show(); }else{ $(this).hide(); }
 });
+$('#main thead tr').show();
+}
+
+$('div.left div.res').on('click','.bt',function(){
+var dt=$.trim($(this).text());
+table_hide(dt);
+});
+$('div.left div.res').on('click','.bt1',function(){
+var dt=$.trim($(this).text());
+table_hide1(dt);
+});
+
+
+$('div.search').on('change','input.sinp',function(){
+var ssel=$('div.search select option:selected').val();
+var stxt=$(this).val();
+var jobc=0;
+var corec=0;
+// alert(stxt+"...."+ssel);
+show_table();
+
+if(stxt){
+var stxt1=new RegExp(stxt,"i");
+$('#main tr').each(function(){
+chk=$.trim($(this).children("td:nth-child("+ssel+")").text());
+str=chk.match(stxt1);
+data2=$.trim($(this).children("td:nth-child(6)").text());
+num=parseInt(data2)?data2:0;
+//alert(chk+"----"+stxt1+"...."+str);
+if(str){ $(this).show(); jobc=jobc+1; corec=corec+parseInt(num); }else{ $(this).hide(); }
+});
+$('#main thead tr').show();
+}else{
+jobc=parseInt(jobsused);
+corec=parseInt(coresused);
+}
+$('div.right div.res span').html("Job's Count : <b>"+jobc+"</b> ___ Core's Count : <b>"+corec+"</b>");
+
+});
+
+$('div.search').on('change','select',function(){
+// alert('reset');
+show_table();
+});
+
+
+
+$("div.dmain1").show();
+$("div.dmain2").hide();
+$('.system').click(function(){ $("div.dmain1").show(); $("div.dmain2").hide(); });
+$('.details').click(function(){ $("div.dmain1").hide(); $("div.dmain2").show(); });
 
 });
 
